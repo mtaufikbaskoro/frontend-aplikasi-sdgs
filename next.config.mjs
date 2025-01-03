81c4ce292c31e32efed8e6bb5cca4104fd81e362
bas <nyxxyr@gmail.com>
2025-01-03 16:54:44 +0700

    adjusting production in subfolder domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: isProduction ? '/astra' : '',
+    assetPrefix: isProduction ? '/astra' : '',
+};
 
 export default nextConfig;
